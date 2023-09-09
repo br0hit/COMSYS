d8ed97f35d39031127ed23ad09a82f1862a50330
rohit <rohit.bannuru@gmail.com>
2023-09-10 02:59:30 +0530

    Cleaned stakcing code

--- text-classification/testing/stacking.py
+++ text-classification/testing/stacking.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
 import xgboost as xgb
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import GradientBoostingClassifier,RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn.ensemble import StackingClassifier
 
-SEED=123
+SEED = 123
 
 file_name = '../preprocessing/vectorization/bm25_vect_with_custom1.npz'
 
@@ -15,11 +15,10 @@ file_name = '../preprocessing/vectorization/bm25_vect_with_custom1.npz'
 loaded_data = np.load(file_name)
 
 # Access and load individual arrays from the loaded data
-X_train = loaded_data['X_train']
+X = loaded_data['X_train']
 
 with open('../data/encoded_labels.txt', 'r') as file:
-    y_train = np.array([int(line.strip()) for line in file])
-
+    y = np.array([int(line.strip()) for line in file])
 
 # Create a list of base models
 base_models = [
@@ -31,4 +30,12 @@ base_models = [
 # Create a stacking ensemble with a meta-model
 stacking_model = StackingClassifier(estimators=base_models, final_estimator=RandomForestClassifier(random_state=SEED, n_estimators=100))
 
+# Use cross-validation to estimate the accuracy of the stacking model
+cv_scores = cross_val_score(stacking_model, X, y, cv=5)  # You can adjust the number of folds (cv) as needed
 
+# Calculate the mean accuracy and standard deviation of the cross-validation scores
+mean_accuracy = np.mean(cv_scores)
+std_accuracy = np.std(cv_scores)
+
+print(f"Mean Accuracy: {mean_accuracy}")
+print(f"Standard Deviation of Accuracy: {std_accuracy}")
